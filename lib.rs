6e92911c1474abc690f5506d04a742ddcb12e68f
Olek <48182795+Sniezka1927@users.noreply.github.com>
2023-11-27 11:48:08 +0100

    Add files via upload

--- lib.rs
+++ lib.rs
@@ -2,6 +2,7 @@
 
 #[ink::contract]
 mod bug {
+    
     #[ink(storage)]
     pub struct Bug {
         last_timestamp: u128,
@@ -12,7 +13,7 @@ mod bug {
         #[ink(constructor)]
         pub fn new() -> Self {
             Self {
-                last_timestamp: u128::default(),
+                last_timestamp: Self::env().block_timestamp() as u128,
                 value: u128::default(),
             }
         }
@@ -27,13 +28,10 @@ mod bug {
             let current_timestamp = self.env().block_timestamp().clone() as u128;
             let timestamp_delta = (current_timestamp - self.last_timestamp) as u128;
 
-            // let value = 1000000000000000000000000000000 * timestamp_delta / 100;
-            let value = (1000000000000000000000000000000 * timestamp_delta) / 1000000000000;
+            let multiplication =1000000000000000000000000000000u128.checked_mul(timestamp_delta).unwrap_or_else(|| panic!("Multiplication overflow"));
+            let division = multiplication.checked_div(1000000000).unwrap_or_else(|| panic!("Division overflow"));
 
-            if value == 0 {
-                ink::env::debug_println!("Value is zero");
-            }
-
+            let value = division;
             self.last_timestamp = current_timestamp as u128;
             self.value = value;
         }
@@ -42,6 +40,7 @@ mod bug {
         pub fn get_timestamps(&self) -> (u128, u128) {
             (self.last_timestamp, self.value)
         }
+
     }
 
     #[cfg(all(test, feature = "e2e-tests"))]
@@ -68,8 +67,7 @@ mod bug {
                 .return_value();
 
             let mut last_timestamp = result.0;
-
-            for n in 1..10 {
+            for n in 1..200 {
                 let msg = build_message::<BugRef>(contract_account_id.clone())
                     .call(|bug| bug.get_timestamps());
                 let result = client
